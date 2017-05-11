266ff960f1b60ef5ee252b66e0646d8a7c078dbb
Niklas W. Micheelsen <niklas@micheelsen.com>
2017-05-11 13:55:46 +0200

    Created ToolTip layout and style Finished Clear and Exit button
    Missing ImportBtn and PushPinCollection 'filling'

--- BingMapsViewer/MainWindow.xaml.cs
+++ BingMapsViewer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,26 +17,49 @@ using System.Windows.Shapes;
 using Microsoft.Maps.MapControl.WPF;
 using Json;
 
-namespace BingMapsViewer
-{
+namespace BingMapsViewer {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public ObservableCollection<Datapoint> PushPinCollection = new ObservableCollection<Datapoint>();
+    public partial class MainWindow : Window {
+        public static ObservableCollection<Datapoint> PushPinCollection { get; set; } =
+            new ObservableCollection<Datapoint>();
 
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
+            ToolTipService.ShowDurationProperty.OverrideMetadata(typeof(DependencyObject),
+                new FrameworkPropertyMetadata(Int32.MaxValue));
 
             PushPinCollection.Add(new Datapoint(
                 new Location(55.732627, 12.342962),
-                DateTime.Now,
+                DateTime.Now.Date.ToString("d", CultureInfo.InvariantCulture),
+                DateTime.Now.ToString("hh:mm:ss", CultureInfo.InvariantCulture),
                 60,
                 4,
                 3000
-                ));
+            ));
+        }
+
+        private void Pushpin_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            // Get a reference to the object that was clicked.
+            var frameworkElement = sender as FrameworkElement;
+            if (frameworkElement != null) {
+                var clickedSearchResult = frameworkElement.DataContext;
+
+                // Do something with it.
+            }
+        }
+
+        private void ImportBtn_OnClick(object sender, RoutedEventArgs e) {
+            // ignored
+        }
+
+        private void ClearBtn_OnClick(object sender, RoutedEventArgs e) {
+            PushPinCollection.Clear();
+        }
+
+        private void ExitBtn_OnClick(object sender, RoutedEventArgs e) {
+            Close();
         }
     }
 }
--- BingMapsViewer/datapoint.cs
+++ BingMapsViewer/datapoint.cs
@@ -3,18 +3,20 @@ using Microsoft.Maps.MapControl.WPF;
 
 namespace BingMapsViewer {
     public class Datapoint {
-        public Datapoint(Location location, DateTime time, int speed, int gear, int rpm) {
+        public Location Location { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Speed { get; set; }
+        public int Gear { get; set; }
+        public int Rpm { get; set; }
+
+        public Datapoint(Location location, string date, string time, int speed, int gear, int rpm) {
             Location = location;
+            Date = date;
             Time = time;
             Speed = speed;
             Gear = gear;
             Rpm = rpm;
         }
-
-        private Location Location { get; set; }
-        private DateTime Time { get; set; }
-        private int Speed { get; set; }
-        private int Gear { get; set; }
-        private int Rpm { get; set; }
     }
 }
