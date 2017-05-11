8d633b3fc3c49a6a8f4510b9c0d88c13b9325540
Rasmus Wejlgaard <zrw@kmd.dk>
2017-05-11 11:29:59 +0200

    Added Datapoint class, and test pin

--- BingMapsViewer/MainWindow.xaml.cs
+++ BingMapsViewer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Maps.MapControl.WPF;
+using Json;
 
 namespace BingMapsViewer
 {
@@ -21,9 +23,19 @@ namespace BingMapsViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Datapoint> PushPinCollection = new ObservableCollection<Datapoint>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            PushPinCollection.Add(new Datapoint(
+                new Location(55.732627, 12.342962),
+                DateTime.Now,
+                60,
+                4,
+                3000
+                ));
         }
     }
 }
