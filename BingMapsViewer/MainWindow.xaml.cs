5b287337a43e566acfb9bacc063f27c090b453b5
RWejlgaard <wejlgaard@protonmail.com>
2017-05-14 19:26:02 +0200

    added prototype of import

--- BingMapsViewer/MainWindow.xaml.cs
+++ BingMapsViewer/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Maps.MapControl.WPF;
 using Json;
+using Microsoft.Win32;
 
 namespace BingMapsViewer {
     /// <summary>
@@ -50,8 +52,76 @@ namespace BingMapsViewer {
             }
         }
 
+        /*
+        #define PID_GPS_LATITUDE 0xA
+        #define PID_GPS_LONGITUDE 0xB
+        #define PID_GPS_ALTITUDE 0xC
+        #define PID_GPS_SPEED 0xD
+        #define PID_GPS_HEADING 0xE
+        #define PID_GPS_SAT_COUNT 0xF
+        #define PID_GPS_TIME 0x10
+        #define PID_GPS_DATE 0x11
+        */
+
         private void ImportBtn_OnClick(object sender, RoutedEventArgs e) {
-            // ignored
+            /*var ofd = new OpenFileDialog {
+                Filter = "CSV Files (*.CSV)|All Files (*.*)"
+            };
+
+            var result = ofd.ShowDialog();
+
+            if (result != false) {
+                ImportData(ofd.FileName);
+            }*/
+
+            ImportData("C:\\Users\\Cosmo\\Downloads\\DAT00004.CSV");
+        }
+
+        class DATA {
+            public static string RPM => "10C";
+            public static string SPEED => "10D";
+            public static string LAT => "A";
+            public static string LNG => "B";
+        }
+
+        private void ImportData(string file) {
+            var lines = File.ReadAllLines(file);
+            for (var i = 0; i <= lines.Length;) {
+                var data = lines[i].Split(',');
+                if (data[1] == DATA.RPM) {
+                    var count = 1;
+
+                    var rpm = int.Parse(data[2]);
+                    var speed = 0;
+                    double lat = 0;
+                    double lng = 0;
+
+
+                    while (lines[i + count].Split(',')[1] != DATA.RPM) {
+                        data = lines[i + count].Split(',');
+
+                        if (data[1] == DATA.SPEED) {
+                            speed = int.Parse(data[2]);
+                        }
+                        else if (data[1] == DATA.LAT) {
+                            lat = double.Parse(data[2].Insert(2, "."));
+                        }
+                        else if (data[1] == DATA.LNG) {
+                            lng = double.Parse(data[2].Insert(2, "."));
+                        }
+
+                        count++;
+                    }
+
+                    if (lng != 0) {
+                        if (lat != 0) {
+                            PushPinCollection.Add(new Datapoint(new Location(lat, lng), "TEST", "TEST", speed, 0, rpm));
+                            MessageBox.Show($"rpm: {rpm}\nspeed: {speed}\nlat: {lat}\nlng: {lng}");
+                            break;
+                        }
+                    }
+                }
+            }
         }
 
         private void ClearBtn_OnClick(object sender, RoutedEventArgs e) {
