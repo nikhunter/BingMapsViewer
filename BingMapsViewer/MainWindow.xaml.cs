a33d8c0efbcbdb17bfa9be181042795885a95ab3
Niklas W. Micheelsen <niklas@micheelsen.com>
2017-06-08 21:44:18 +0200

    Imported some NuGet packages to allow MySql and Json functions Implemented the ability to fetch Json data from the Raven database

--- BingMapsViewer/MainWindow.xaml.cs
+++ BingMapsViewer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,6 +10,8 @@ using System.Device.Location;
 using System.Globalization;
 using Microsoft.Maps.MapControl.WPF;
 using Microsoft.Win32;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
 
 namespace BingMapsViewer {
     /// <summary>
@@ -18,6 +22,8 @@ namespace BingMapsViewer {
             new ObservableCollection<Datapoint>(); // Collection of Map  pins, uses custom Datapoint class as type
 
         public static MapLayer PolyLineLayer = new MapLayer(); // Layer used only for MapPolyLines for easier cleaning
+
+        private const string ConnectionString = "Server=raven-gps.com;Database=raven;Uid=root;Pwd=Raven123";
 
         public MainWindow() {
             InitializeComponent();
@@ -44,23 +50,89 @@ namespace BingMapsViewer {
             }
             Map.Children.Add(PolyLineLayer); // Adds the PolyLineLayer to the main Map as a child
         }
-        
+
+        private void ImportFromSqlBtn_OnClick(object sender, RoutedEventArgs e) {
+            int parsedValue;
+            if (!int.TryParse(SqlIdBox.Text, out parsedValue)) {
+                MessageBox.Show("Invalid number in text field");
+                return;
+            }
+            ImportJson(SqlIdBox.Text);
+            ImportFromSqlBtn.IsEnabled = false;
+        }
+
+        private void ImportJson(string id) {
+            MySqlConnection connection = new MySqlConnection(ConnectionString);
+            DataTable dt = new DataTable();
+            connection.Open();
+
+            try {
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = $"SELECT * FROM trips WHERE id='{id}'";
+
+                // TODO Make this into a separate method
+                using (MySqlDataReader dr = command.ExecuteReader()) {
+                    dt.Load(dr);
+                    foreach (DataRow row in dt.Rows) {
+                        try {
+                            var logs = row["log_file"].ToString();
+
+                            // TODO Fetch coordinates of first and last 'result'
+                            List<RootObject> results = JsonConvert.DeserializeObject<List<RootObject>>(logs);
+
+                            int lineCount = 1;
+                            foreach (var line in results) {
+                                double lat = double.Parse(line.Latitude, CultureInfo.InvariantCulture);
+                                double lng = double.Parse(line.Longitude, CultureInfo.InvariantCulture);
+                                var date = line.Date;
+                                var time = line.Time;
+                                var speed = int.Parse(line.Speed);
+                                var rpm = int.Parse(line.Rpm);
+
+                                if (PushPinCollection.Count > 0 &&
+                                    CalculateDistance(PushPinCollection[PushPinCollection.Count - 1].Location,
+                                        new Location(lat, lng)) > 100 &&
+                                    CalculateDistance(PushPinCollection[PushPinCollection.Count - 1].Location,
+                                        new Location(lat, lng)) < 1000) {
+                                    PushPinCollection.Add(new Datapoint(new Location(lat, lng), date, time, speed,
+                                        rpm));
+                                }
+                                else if (PushPinCollection.Count == 0) {
+                                    PushPinCollection.Add(
+                                        new Datapoint(new Location(lat, lng), date, time, speed, rpm));
+                                }
+                                lineCount = lineCount + 1;
+                            }
+                        }
+                        catch {
+                            // ignored
+                        }
+                    }
+                }
+            }
+            catch {
+                // ignored
+            }
+            DrawLines();
+        }
+
         // Opens a file dialog where you can select a CSV file
-        private void ImportBtn_OnClick(object sender, RoutedEventArgs e) {
+        private void ImportFromFileBtn_OnClick(object sender, RoutedEventArgs e) {
             var ofd = new OpenFileDialog {
                 Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*",
             };
 
             var result = ofd.ShowDialog();
 
-            if (result != false) { // If file is selected, start data import and disable ImportBtn
+            if (result != false) {
+                // If file is selected, start data import and disable ImportBtn
                 ImportData(ofd.FileName);
-                ImportBtn.IsEnabled = false;
+                ImportFromFileBtn.IsEnabled = false;
             }
         }
 
         /*
-                PID Codes
+            PID Codes
             Rpm     =   10C
             Speed   =   10D
             Lat     =   A
@@ -68,17 +140,14 @@ namespace BingMapsViewer {
             Time    =   10
             Date    =   11
         */
-        private void ImportData(string file)
-        {
+        private void ImportData(string file) {
             // load each file line into array as element
             var lines = File.ReadAllLines(file);
             var date = "";
-            for (var i = 0; i < lines.Length;)
-            {
+            for (var i = 0; i < lines.Length;) {
                 // splits line into array, datatype on [1], value on [2]
                 var data = lines[i].Split(',');
-                if (data[1] == "10C")
-                {
+                if (data[1] == "10C") {
                     // Count used for running through next lines until the next 0x10c
                     var count = 1;
 
@@ -89,17 +158,13 @@ namespace BingMapsViewer {
                     double lng = 0;
                     var time = "";
 
-
-                    try
-                    {
+                    try {
                         // Check if file starts by logging RPM (0x10c)
-                        while (lines[i + count].Split(',')[1] != "10C")
-                        {
+                        while (lines[i + count].Split(',')[1] != "10C") {
                             data = lines[i + count].Split(',');
 
                             // Fill previously declared variables with corresponding data
-                            switch (data[1])
-                            {
+                            switch (data[1]) {
                                 case "10D":
                                     speed = int.Parse(data[2]);
                                     break;
@@ -133,15 +198,13 @@ namespace BingMapsViewer {
                             count++;
                         }
                     }
-                    catch (IndexOutOfRangeException)
-                    {
+                    catch (IndexOutOfRangeException) {
                         // import is probably at the end of the file, so we escape the loop here
                         break;
                     }
 
                     // If dataset doesn't have gps coordinates, discard it
-                    if (lng != 0 && lat != 0)
-                    {
+                    if (lng != 0 && lat != 0) {
                         // TODO Make this threshold check prettier
 
                         // if distance is greater than 100 meter and under 1000, set a pin, otherwise it's too close or a GPS glitch
@@ -150,12 +213,10 @@ namespace BingMapsViewer {
                             CalculateDistance(PushPinCollection[PushPinCollection.Count - 1].Location,
                                 new Location(lat, lng)) > 100 &&
                             CalculateDistance(PushPinCollection[PushPinCollection.Count - 1].Location,
-                                new Location(lat, lng)) < 1000)
-                        {
+                                new Location(lat, lng)) < 1000) {
                             PushPinCollection.Add(new Datapoint(new Location(lat, lng), date, time, speed, rpm));
                         }
-                        else if (PushPinCollection.Count == 0)
-                        {
+                        else if (PushPinCollection.Count == 0) {
                             PushPinCollection.Add(
                                 new Datapoint(new Location(lat, lng), date, time, speed, rpm));
                         }
@@ -165,6 +226,29 @@ namespace BingMapsViewer {
                 }
             }
             DrawLines();
+        }
+
+        public class RootObject {
+            [JsonProperty(PropertyName = "DeltaTime")]
+            public string DeltaTime { get; set; }
+
+            [JsonProperty(PropertyName = "Rpm")]
+            public string Rpm { get; set; }
+
+            [JsonProperty(PropertyName = "Speed")]
+            public string Speed { get; set; }
+
+            [JsonProperty(PropertyName = "Lat")]
+            public string Latitude { get; set; }
+
+            [JsonProperty(PropertyName = "Lng")]
+            public string Longitude { get; set; }
+
+            [JsonProperty(PropertyName = "Date")]
+            public string Date { get; set; }
+
+            [JsonProperty(PropertyName = "Time")]
+            public string Time { get; set; }
         }
 
         // Returns the distance (in metric meters) between two locations
@@ -187,7 +271,9 @@ namespace BingMapsViewer {
             PolyLineLayer.Children.Clear();
             Map.Children.Remove(PolyLineLayer);
 
-            ImportBtn.IsEnabled = true; // Renables the ImportBtn because now the user may import data again
+            // Renables the import buttons because now the user may import data again
+            ImportFromFileBtn.IsEnabled = true;
+            ImportFromSqlBtn.IsEnabled = true;
         }
 
         // Closes the program
